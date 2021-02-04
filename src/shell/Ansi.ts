eb3d4f992ce754ce18fc7187d53e96b94572fbe2
Barry McAndrews <bmcandrews3@hotmail.com>
2021-02-04 12:16:11 -0500

    rename constantt

--- src/shell/Ansi.ts
+++ src/shell/Ansi.ts
@@ -1,41 +1,45 @@
-const ESC = '\u001B[';
+const ESC = '\u001B';
+const CSI = ESC + '[';
 const OSC = '\u001B]';
 const SEP = ';';
 const BEL = '\u0007';
 
-export const CURSOR_UP = ESC + 'A';
-export const CURSOR_DOWN = ESC + 'B';
-export const CURSOR_FORWARD = ESC + 'C';
-export const CURSOR_BACKWARDS = ESC + 'D';
+export const CURSOR_UP = CSI + 'A';
+export const CURSOR_DOWN = CSI + 'B';
+export const CURSOR_FORWARD = CSI + 'C';
+export const CURSOR_BACKWARDS = CSI + 'D';
 
-export let cursorUp = (count = 1) => ESC + count + 'A';
-export let cursorDown = (count = 1) => ESC + count + 'B';
-export let cursorForward = (count = 1) => ESC + count + 'C';
-export let cursorBackward = (count = 1) => ESC + count + 'D';
+export let cursorUp = (count = 1) => CSI + count + 'A';
+export let cursorDown = (count = 1) => CSI + count + 'B';
+export let cursorForward = (count = 1) => CSI + count + 'C';
+export let cursorBackward = (count = 1) => CSI + count + 'D';
 
-export let cursorLeft = ESC + 'G';
-export let cursorSavePosition = ESC + 's';
-export let cursorRestorePosition = ESC + 'u';
-export let cursorGetPosition = ESC + '6n';
-export let cursorNextLine = ESC + 'E';
-export let cursorPrevLine = ESC + 'F';
-export let cursorHide = ESC + '?25l';
-export let cursorShow = ESC + '?25h';
+export let cursorLeft = CSI + 'G';
+export let cursorSavePosition = CSI + 's';
+export let cursorRestorePosition = CSI + 'u';
+export let cursorGetPosition = CSI + '6n';
+export let cursorNextLine = CSI + 'E';
+export let cursorPrevLine = CSI + 'F';
+export let cursorHide = CSI + '?25l';
+export let cursorShow = CSI + '?25h';
 
-export let eraseEndLine = ESC + 'K';
-export let eraseStartLine = ESC + '1K';
-export let eraseLine = ESC + '2K';
+export let eraseEndLine = CSI + 'K';
+export let eraseStartLine = CSI + '1K';
+export let eraseLine = CSI + '2K';
 
-export let eraseDown = ESC + 'J';
-export let eraseUp = ESC + '1J';
-export let eraseScreen = ESC + '2J';
-export let scrollUp = ESC + 'S';
-export let scrollDown = ESC + 'T';
+export let eraseDown = CSI + 'J';
+export let eraseUp = CSI + '1J';
+export let eraseScreen = CSI + '2J';
+export let scrollUp = CSI + 'S';
+export let scrollDown = CSI + 'T';
 
 export let clearScreen = '\u001Bc\n';
-export let clearTerminal = `${eraseScreen}${ESC}3J${ESC}H`;
+export let clearTerminal = `${eraseScreen}${CSI}3J${CSI}H`;
 
 export let beep = BEL;
+
+export let alternateScreen = CSI + '?1049h';
+export let normalScreen = CSI + '?1049l';
 
 
 export function cursorTo(x: any, y: any) {
@@ -44,10 +48,10 @@ export function cursorTo(x: any, y: any) {
   }
 
   if (typeof y !== 'number') {
-    return ESC + (x + 1) + 'G';
+    return CSI + (x + 1) + 'G';
   }
 
-  return ESC + (y + 1) + ';' + (x + 1) + 'H';
+  return CSI + (y + 1) + ';' + (x + 1) + 'H';
 }
 
 export function cursorMove(x: any, y: any) {
@@ -58,15 +62,15 @@ export function cursorMove(x: any, y: any) {
   let ret = '';
 
   if (x < 0) {
-    ret += ESC + (-x) + 'D';
+    ret += CSI + (-x) + 'D';
   } else if (x > 0) {
-    ret += ESC + x + 'C';
+    ret += CSI + x + 'C';
   }
 
   if (y < 0) {
-    ret += ESC + (-y) + 'A';
+    ret += CSI + (-y) + 'A';
   } else if (y > 0) {
-    ret += ESC + y + 'B';
+    ret += CSI + y + 'B';
   }
 
   return ret;
